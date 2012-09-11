3aaf16ec0c8dc50ee1e20915bd72346aa2c56409
Sergio Grau Puerto <sergio.graupuerto@it.ox.ac.uk>
2012-09-11 17:26:54 +0100

    Minor modification input files

--- keywords/keywords.py
+++ keywords/keywords.py
@@ -17,11 +17,13 @@ def keywords_and_ngrams(input, nKeywords=100, thresholdLL=19, nBigrams=25, thres
     fdistText = defaultdict(int)
     sumText = 0
     listWords = []
+    
     listSentences = []
     if isinstance(input,str):
         listSentences = [input]
     else:
         listSentences = input
+    
     for line in listSentences:
         for w in line.split():
             w = w.lower()
