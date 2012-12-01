571602a92196a1ddb9ef693d06972914a69d7a87
joddie <jonxfield@gmail.com>
2012-12-01 01:09:28 +0000

    basic script to setup rabbitmq

---
+++ web/setup_rabbitmq.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo '* Checking for rabbitmq ...'
+rabbitmqctl status || exit 1
+
+echo "* Creating 'spindle' vhost"
+rabbitmqctl create_vhost spindle || exit 1
+
+echo "* Creating 'spindle' user"
+printf "Enter password: "
+read -s pw
+
+rabbitmqctl add_user spindle "$pw" || exit 1
+
+echo "* Setting permissions for 'spindle' on vhost 'spindle'"
+rabbitmqctl set_permissions -p spindle spindle '.*' '.*' '.*' || exit 1
+
+echo '* Done!'
+exit 0
